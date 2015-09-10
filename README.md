9119f3e229db13ca1cdb4958c40cf1f02ed63e24
roylee0704 <roylee0704@gmail.com>
2015-09-10 21:49:22 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+##Introduction
+
+Py-script that basically `READ` from `NodeLink` database table and translate the row datas into `neo4j` cypher language.
+
+
+
+#Upload
+
+To upload the cypher scripts, simple import it via `neo4j-shell` utility.
+
+```sh
+$ neo4j-shell --file ../your/path/to/file/nodelink.cyp
+
+```
+
---
+++ normad.py
@@ -0,0 +1,28 @@
+import MySQLdb
+
+db = MySQLdb.connect(host='localhost',
+                     user='root',
+                     passwd='root123',
+                     db='sam_new')
+cur = db.cursor()
+
+#create( (:Person {name: 'Baby'})-[:DRIVE]->(:Car {name: 'Honda'}) )
+def create_relationship():
+    cur.execute('SELECT * FROM NODELINK')
+
+    outf = open('nodelink.cyp', 'w')
+    for row in cur.fetchall():
+        cql = 'CREATE( (:Node {NODEID: ' + str(row[0]) + '})-[:' + str(row[2]) + ']->(:Node {NODEID:' + str(row[1]) + '}) )' 
+        outf.write(cql + '\n')
+    
+    outf.close()
+
+
+def main():
+
+    #create relationship
+    create_relationship()
+
+
+if __name__ == '__main__':
+      main()
